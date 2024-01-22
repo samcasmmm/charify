977a933837d963a1e07715430dbb33a084bf1cbb
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-22 20:06:27 +0530

    uuidv4.js

---
+++ client/src/utils/uuidc4.ts
@@ -0,0 +1,10 @@
+export default function uuidv4(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+    /[xy]/g,
+    function (c): string {
+      const r: number = (Math.random() * 16) | 0,
+        v: number = c === 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    }
+  );
+}
