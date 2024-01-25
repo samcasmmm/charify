0f12f1a16944a9b73c7d2029be9ab7224aff0a28
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-25 20:32:19 +0530

    transition.ts

---
+++ client/src/components/animate/variants/transition.ts
@@ -0,0 +1,25 @@
+interface TransitionProps {
+  duration?: number;
+  ease?: [number, number, number, number];
+}
+
+export const varTranHover = (props?: TransitionProps) => {
+  const duration = props?.duration || 0.32;
+  const ease = props?.ease || [0.43, 0.13, 0.23, 0.96];
+
+  return { duration, ease };
+};
+
+export const varTranEnter = (props?: TransitionProps) => {
+  const duration = props?.duration || 0.64;
+  const ease = props?.ease || [0.43, 0.13, 0.23, 0.96];
+
+  return { duration, ease };
+};
+
+export const varTranExit = (props?: TransitionProps) => {
+  const duration = props?.duration || 0.48;
+  const ease = props?.ease || [0.43, 0.13, 0.23, 0.96];
+
+  return { duration, ease };
+};
