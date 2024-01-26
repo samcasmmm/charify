4cfbfef9b4b12c9e949151ac8eed3d2cf66f3b73
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-26 22:46:44 +0530

    vounce.tsx

--- client/src/components/animate/variants/transition.ts
+++ client/src/components/animate/variants/transition.ts
@@ -1,6 +1,10 @@
 export interface TransitionProps {
   duration?: number;
+  durationIn?: number | undefined;
+  durationOut?: number | undefined;
+  easeIn?: number[] | undefined;
   ease?: [number, number, number, number];
+  easeOut?: number[] | undefined;
 }
 
 export const varTranHover = (props?: TransitionProps) => {
@@ -12,9 +16,11 @@ export const varTranHover = (props?: TransitionProps) => {
 
 export const varTranEnter = (props?: TransitionProps) => {
   const duration = props?.duration || 0.64;
+  const durationIn = props?.durationIn || 0.64;
   const ease = props?.ease || [0.43, 0.13, 0.23, 0.96];
+  const easeIn = props?.easeIn;
 
-  return { duration, ease };
+  return { duration, durationIn, ease, easeIn };
 };
 
 export const varTranExit = (props?: TransitionProps) => {
