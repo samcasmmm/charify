33e2359df81a61050fe3242b8781ecb82cba41d5
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-27 19:18:04 +0530

    slide.tsx

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   Box,
   Fab,
@@ -6,7 +7,7 @@ import {
   Stack,
   TextField,
   Tooltip,
-} from "@mui/material";
+} from '@mui/material';
 import {
   Camera,
   File,
@@ -16,85 +17,96 @@ import {
   Smiley,
   Sticker,
   User,
-} from "phosphor-react";
-import { useTheme, styled } from "@mui/material/styles";
-import React from "react";
-import { useSearchParams } from "react-router-dom";
-import useResponsive from "../../hooks/useResponsive";
+} from 'phosphor-react';
+import { useTheme, styled } from '@mui/material/styles';
+import { useSearchParams } from 'react-router-dom';
+import useResponsive from '../../hooks/useResponsive';
 
-import data from "@emoji-mart/data";
-import Picker from "@emoji-mart/react";
+import data from '@emoji-mart/data';
+import Picker from '@emoji-mart/react';
+
+interface Action {
+  color: string;
+  icon: JSX.Element;
+  y: number;
+  title: string;
+}
+
+interface ChatInputProps {
+  openPicker: boolean;
+  setOpenPicker: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
 const StyledInput = styled(TextField)(({ theme }) => ({
-  "& .MuiInputBase-input": {
-    paddingTop: "12px !important",
-    paddingBottom: "12px !important",
+  '& .MuiInputBase-input': {
+    paddingTop: '12px !important',
+    paddingBottom: '12px !important',
   },
 }));
 
-const Actions = [
+const Actions: Action[] = [
   {
-    color: "#4da5fe",
+    color: '#4da5fe',
     icon: <Image size={24} />,
     y: 102,
-    title: "Photo/Video",
+    title: 'Photo/Video',
   },
   {
-    color: "#1b8cfe",
+    color: '#1b8cfe',
     icon: <Sticker size={24} />,
     y: 172,
-    title: "Stickers",
+    title: 'Stickers',
   },
   {
-    color: "#0172e4",
+    color: '#0172e4',
     icon: <Camera size={24} />,
     y: 242,
-    title: "Image",
+    title: 'Image',
   },
   {
-    color: "#0159b2",
+    color: '#0159b2',
     icon: <File size={24} />,
     y: 312,
-    title: "Document",
+    title: 'Document',
   },
   {
-    color: "#013f7f",
+    color: '#013f7f',
     icon: <User size={24} />,
     y: 382,
-    title: "Contact",
+    title: 'Contact',
   },
 ];
 
-const ChatInput = ({ openPicker, setOpenPicker }) => {
-  const [openActions, setOpenActions] = React.useState(false);
+const ChatInput: React.FC<ChatInputProps> = ({ openPicker, setOpenPicker }) => {
+  const [openActions, setOpenActions] = useState(false);
 
   return (
     <StyledInput
       fullWidth
-      placeholder="Write a message..."
-      variant="filled"
+      placeholder='Write a message...'
+      variant='filled'
       InputProps={{
         disableUnderline: true,
         startAdornment: (
-          <Stack sx={{ width: "max-content" }}>
+          <Stack sx={{ width: 'max-content' }}>
             <Stack
               sx={{
-                position: "relative",
-                display: openActions ? "inline-block" : "none",
+                position: 'relative',
+                display: openActions ? 'inline-block' : 'none',
               }}
             >
               {Actions.map((el) => (
-                <Tooltip placement="right" title={el.title}>
+                <Tooltip placement='right' title={el.title} key={el.title}>
                   <Fab
                     onClick={() => {
                       setOpenActions(!openActions);
                     }}
                     sx={{
-                      position: "absolute",
+                      position: 'absolute',
                       top: -el.y,
                       backgroundColor: el.color,
                     }}
-                    aria-label="add"
+                    aria-label='add'
                   >
                     {el.icon}
                   </Fab>
@@ -102,7 +114,8 @@ const ChatInput = ({ openPicker, setOpenPicker }) => {
               ))}
             </Stack>
 
-            <InputAdornment>
+            {/* Specify the `component` prop for InputAdornment */}
+            <InputAdornment component='div'>
               <IconButton
                 onClick={() => {
                   setOpenActions(!openActions);
@@ -114,7 +127,8 @@ const ChatInput = ({ openPicker, setOpenPicker }) => {
           </Stack>
         ),
         endAdornment: (
-          <Stack sx={{ position: "relative" }}>
+          <Stack sx={{ position: 'relative' }}>
+            {/* Specify the `component` prop for InputAdornment */}
             <InputAdornment>
               <IconButton
                 onClick={() => {
@@ -131,43 +145,43 @@ const ChatInput = ({ openPicker, setOpenPicker }) => {
   );
 };
 
-const Footer = () => {
+const Footer: React.FC = () => {
   const theme = useTheme();
 
-  const isMobile = useResponsive("between", "md", "xs", "sm");
+  const isMobile = useResponsive('between', 'md', 'xs', 'sm');
 
   const [searchParams] = useSearchParams();
 
-  const [openPicker, setOpenPicker] = React.useState(false);
+  const [openPicker, setOpenPicker] = useState(false);
   return (
     <Box
       sx={{
-        position: "relative",
-        backgroundColor: "transparent !important",
+        position: 'relative',
+        backgroundColor: 'transparent !important',
       }}
     >
       <Box
         p={isMobile ? 1 : 2}
-        width={"100%"}
+        width={'100%'}
         sx={{
           backgroundColor:
-            theme.palette.mode === "light"
-              ? "#F8FAFF"
+            theme.palette.mode === 'light'
+              ? '#F8FAFF'
               : theme.palette.background,
-          boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
         }}
       >
-        <Stack direction="row" alignItems={"center"} spacing={isMobile ? 1 : 3}>
-          <Stack sx={{ width: "100%" }}>
+        <Stack direction='row' alignItems={'center'} spacing={isMobile ? 1 : 3}>
+          <Stack sx={{ width: '100%' }}>
             <Box
               style={{
                 zIndex: 10,
-                position: "fixed",
-                display: openPicker ? "inline" : "none",
+                position: 'fixed',
+                display: openPicker ? 'inline' : 'none',
                 bottom: 81,
                 right: isMobile
                   ? 20
-                  : searchParams.get("open") === "true"
+                  : searchParams.get('open') === 'true'
                   ? 420
                   : 100,
               }}
@@ -190,12 +204,12 @@ const Footer = () => {
             }}
           >
             <Stack
-              sx={{ height: "100%" }}
-              alignItems={"center"}
-              justifyContent="center"
+              sx={{ height: '100%' }}
+              alignItems={'center'}
+              justifyContent='center'
             >
               <IconButton>
-                <PaperPlaneTilt color="#ffffff" />
+                <PaperPlaneTilt color='#ffffff' />
               </IconButton>
             </Stack>
           </Box>
