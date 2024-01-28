4fcd3b7d908b676e3384d8d5a10cd1c436c73a95
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-28 17:36:28 +0530

    sidebar.jsx

--- client/src/layouts/dashboard/index.jsx
+++ client/src/layouts/dashboard/index.jsx
@@ -1,17 +1,68 @@
 import { Outlet } from 'react-router-dom';
 import { Box, Stack, IconButton, Divider, Avatar } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+import { useTheme, styled } from '@mui/material/styles';
 import { Gear } from 'phosphor-react';
-
-import Logo from '../../assets/Images/logo.png';
-import { Nav_Buttons } from '../../data';
 import { useState } from 'react';
 import { faker } from '@faker-js/faker';
 import { Switch } from '@mui/material';
 
+import Logo from '../../assets/Images/logo.png';
+import { Nav_Buttons } from '../../data';
+import useSettings from './../../hooks/useSettings';
+
+const AntSwitch = styled(Switch)(({ theme }) => {
+  const WIDTH = 40;
+  const HEIGHT = WIDTH / 2;
+  return {
+    width: WIDTH,
+    height: HEIGHT,
+    padding: 0,
+    display: 'flex',
+    '&:active': {
+      '& .MuiSwitch-thumb': {
+        width: 15,
+      },
+      '& .MuiSwitch-switchBase.Mui-checked': {
+        transform: 'translateX(9px)',
+      },
+    },
+    '& .MuiSwitch-switchBase': {
+      padding: 2,
+      '&.Mui-checked': {
+        transform: `translateX(${HEIGHT}px)`,
+        color: '#fff',
+        '& + .MuiSwitch-track': {
+          opacity: 1,
+          backgroundColor:
+            theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+        },
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+      width: HEIGHT - 4,
+      height: HEIGHT - 4,
+      borderRadius: (HEIGHT - 4) / 2,
+      transition: theme.transitions.create(['width'], {
+        duration: 200,
+      }),
+    },
+    '& .MuiSwitch-track': {
+      borderRadius: HEIGHT / 2,
+      opacity: 1,
+      backgroundColor:
+        theme.palette.mode === 'dark'
+          ? 'rgba(255,255,255,.35)'
+          : 'rgba(0,0,0,.25)',
+      boxSizing: 'border-box',
+    },
+  };
+});
+
 const DashboardLayout = () => {
   const theme = useTheme();
   const [selected, setSelected] = useState(0);
+  const { onToggleMode } = useSettings();
 
   return (
     <>
@@ -62,7 +113,13 @@ const DashboardLayout = () => {
                     key={item.index}
                     sx={{
                       width: 'max-content',
-                      color: `${item.index === selected ? '#fff' : '#000'}`,
+                      color: `${
+                        item.index === selected
+                          ? '#fff'
+                          : theme.palette.mode === 'light'
+                          ? '#000'
+                          : theme.palette.text.primary
+                      }`,
                     }}
                     onClick={() => setSelected(item.index)}
                   >
@@ -82,7 +139,13 @@ const DashboardLayout = () => {
                 <IconButton
                   sx={{
                     width: 'max-content',
-                    color: `${3 === selected ? '#fff' : '#000'}`,
+                    color: `${
+                      3 === selected
+                        ? '#fff'
+                        : theme.palette.mode === 'light'
+                        ? '#000'
+                        : theme.palette.text.primary
+                    }`,
                   }}
                   onClick={() => setSelected(3)}
                 >
@@ -92,7 +155,7 @@ const DashboardLayout = () => {
             </Stack>
           </Stack>
           <Stack direction={'column'} alignItems={'center'} spacing={4}>
-            <Switch defaultChecked />
+            <AntSwitch onChange={onToggleMode} defaultChecked />
             <Avatar src={faker.image.avatar()} alt='avator' />
           </Stack>
         </Stack>
