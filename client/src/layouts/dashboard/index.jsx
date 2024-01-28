5f6a3597ee619b918217f62db2c0504e3a564e78
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-28 17:04:19 +0530

    sidebar.jsx

--- client/src/layouts/dashboard/index.jsx
+++ client/src/layouts/dashboard/index.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import { Box, Stack, IconButton, Divider } from '@mui/material';
+import { Box, Stack, IconButton, Divider, Avatar } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { Gear } from 'phosphor-react';
 
 import Logo from '../../assets/Images/logo.png';
 import { Nav_Buttons } from '../../data';
 import { useState } from 'react';
+import { faker } from '@faker-js/faker';
+import { Switch } from '@mui/material';
 
 const DashboardLayout = () => {
   const theme = useTheme();
@@ -24,67 +26,74 @@ const DashboardLayout = () => {
         <Stack
           direction={'column'}
           alignItems={'center'}
-          sx={{ width: '100%' }}
+          justifyContent={'space-between'}
+          sx={{ width: '100%', height: '97vh' }}
           spacing={3}
-          mt={5}
+          pt={5}
         >
-          <Box
-            sx={{
-              backgroundColor: '#CCCCCC20',
-              height: 64,
-              width: 64,
-              borderRadius: 2.5,
-              boxShadow: 1,
-            }}
-          >
-            <img src={Logo} alt='App Logo' style={{ filter: 'tint(black)' }} />
-          </Box>
-          <Stack
-            sx={{ width: 'max-content' }}
-            direction={'column'}
-            alignItems={'center'}
-            spacing={2}
-          >
-            {Nav_Buttons.map((item) => (
+          <Stack alignItems={'center'} spacing={4}>
+            <Box
+              sx={{
+                backgroundColor: '#CCCCCC20',
+                height: 64,
+                width: 64,
+                borderRadius: 2.5,
+                boxShadow: 1,
+              }}
+            >
+              <img src={Logo} alt='App Logo' />
+            </Box>
+            <Stack
+              sx={{ width: 'max-content' }}
+              direction={'column'}
+              alignItems={'center'}
+              spacing={2}
+            >
+              {Nav_Buttons.map((item) => (
+                <Box
+                  sx={{
+                    backgroundColor: `${
+                      item.index === selected && theme.palette.primary.main
+                    }`,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton
+                    key={item.index}
+                    sx={{
+                      width: 'max-content',
+                      color: `${item.index === selected ? '#fff' : '#000'}`,
+                    }}
+                    onClick={() => setSelected(item.index)}
+                  >
+                    {item.icon}
+                  </IconButton>
+                </Box>
+              ))}
+              <Divider />
               <Box
                 sx={{
                   backgroundColor: `${
-                    item.index === selected && theme.palette.primary.main
+                    3 === selected && theme.palette.primary.main
                   }`,
                   borderRadius: 1.5,
                 }}
               >
                 <IconButton
-                  key={item.index}
                   sx={{
                     width: 'max-content',
-                    color: `${item.index === selected ? '#fff' : '#000'}`,
+                    color: `${3 === selected ? '#fff' : '#000'}`,
                   }}
-                  onClick={() => setSelected(item.index)}
+                  onClick={() => setSelected(3)}
                 >
-                  {item.icon}
+                  <Gear />
                 </IconButton>
               </Box>
-            ))}
-            <Divider />
-            <Box
-              sx={{
-                backgroundColor: `${
-                  3 === selected && theme.palette.primary.main
-                }`,
-                borderRadius: 1.5,
-              }}
-            >
-              <IconButton
-                sx={{
-                  width: 'max-content',
-                  color: `${3 === selected ? '#fff' : '#000'}`,
-                }}
-                onClick={() => setSelected(3)}
-              >
-                <Gear />
-              </IconButton>
-            </Box>
+            </Stack>
+          </Stack>
+          <Stack direction={'column'} alignItems={'center'} spacing={4}>
+            <Switch defaultChecked />
+            <Avatar src={faker.image.avatar()} alt='avator' />
           </Stack>
         </Stack>
       </Box>
