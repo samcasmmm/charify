04b0937263871cb35cdec4ed99d42eabbb86f4a9
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-31 20:17:01 +0530

    chats.jsx

--- client/src/pages/dashboard/Chats.jsx
+++ client/src/pages/dashboard/Chats.jsx
@@ -8,11 +8,41 @@ import {
   Button,
   Divider,
   Avatar,
+  Badge,
 } from '@mui/material';
 import { styled, alpha } from '@mui/material/styles';
 import { CircleDashed } from 'phosphor-react';
 import { MagnifyingGlass, ArchiveBox } from 'phosphor-react';
 import { faker } from '@faker-js/faker';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
 
 const ChatElement = () => {
   return (
@@ -24,8 +54,22 @@ const ChatElement = () => {
       }}
       p={2}
     >
-      <Stack></Stack>
-      <Avatar src={faker} />
+      <Stack
+        direction={'row'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+      >
+        <Stack direction={'row'} spacing={2}>
+          <StyledBadge
+            overlap='circular'
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            variant='dot'
+          >
+            <Avatar src={faker.image.avatar()} />
+          </StyledBadge>
+        </Stack>
+        <Stack></Stack>
+      </Stack>
     </Box>
   );
 };
