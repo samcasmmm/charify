3e22b968af6072ad2333ff990d9965380fdb36dc
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-02-01 21:01:34 +0530

    revamp

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './rootReducer';
+import { rootReducer } from './rootReducer.ts';
 import { useApi } from './useApi.ts';
 const Store = configureStore({
   reducer: rootReducer,
