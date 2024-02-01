fa8d04047eafece9596a282a820b1bfd2906addf
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-02-01 21:25:58 +0530

    revamp

--- client/src/layout/Desktop.tsx
+++ client/src/layout/Desktop.tsx
@@ -1,7 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
 type Props = {};
 
 const Desktop = (props: Props) => {
-  return <div>Desktop</div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Desktop;
--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,12 +1,18 @@
 import { lazy } from 'react';
 
-const Home = lazy(() => import('@/pages/home/About'));
+const Home = lazy(() => import('@/pages/home/Home'));
+const About = lazy(() => import('@/pages/about/About'));
 
 const coreRoutes = [
   {
     path: '/',
     title: 'Home',
     component: Home,
+  },
+  {
+    path: '/about',
+    title: 'About',
+    component: About,
   },
 ];
 
