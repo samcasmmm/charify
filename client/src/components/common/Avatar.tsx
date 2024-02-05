c5c2c4e4cbdef60d21c1c0f64ad834303b5997c4
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-02-05 20:01:04 +0530

    Avatar.tsx

--- client/src/components/common/Avatar.tsx
+++ client/src/components/common/Avatar.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import {
+  Avatar as AvatarMain,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar";
 
-const Avatar = () => {
-  return <div>Avatar</div>;
+type AvatarProps = {
+  label?: string;
+  src?: string;
+  alt?: string;
+};
+
+const Avatar: React.FC<AvatarProps> = ({ label, src, alt }) => {
+  return (
+    <AvatarMain>
+      <AvatarImage
+        src={label ? label : "https://github.com/shadcn.png"}
+        alt={alt ? alt : "@shadcn"}
+      />
+      <AvatarFallback>{label ? label : "CN"}</AvatarFallback>
+    </AvatarMain>
+  );
 };
 
 export default Avatar;
