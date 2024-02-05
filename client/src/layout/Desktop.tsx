991551ea985dbebfbb316b067b14a9e88c62ab21
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-02-05 20:25:02 +0530

    Avatar.tsx

--- client/src/layout/Desktop.tsx
+++ client/src/layout/Desktop.tsx
@@ -5,7 +5,9 @@ const Desktop = () => {
   return (
     <div className="flex h-screen w-full flex-row">
       <Sidebar />
-      <div>Chats</div>
+      <div className="w-[300px] bg-slate-300/30 dark:bg-slate-800/70">
+        Chats
+      </div>
       <Outlet />
     </div>
   );
