ccfee5727501baa3f6b59c5c7105307d761d67b0
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-02-06 19:56:45 +0530

    chats.tsx

--- client/src/components/chats/Chats.tsx
+++ client/src/components/chats/Chats.tsx
@@ -1,12 +1,12 @@
-import { Avatar } from "@/components/index";
+import { Avatar, Icon } from "@/components/";
+import { CircleDashed } from "phosphor-react";
 const Chats: React.FC = () => {
   return (
     <div className="w-[300px] bg-slate-300/30 p-4 dark:bg-slate-800/70">
-      Chats
-      <div className="flex flex-row justify-between">
-        <div className="">asdf</div>
-
-        <div className="">asdfA</div>
+      <div className="flex flex-row items-center justify-between">
+        <h4 className="text-2xl font-bold">Chats</h4>
+        <div className="">asdfAAvatar</div>
+        <Icon Icon={CircleDashed} size={24} />
       </div>
     </div>
   );
