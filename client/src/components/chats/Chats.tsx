096c7636b5bd1df2a872c929a7dd787565ab6fc9
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-02-06 20:04:21 +0530

    chats.tsx

--- client/src/components/chats/Chats.tsx
+++ client/src/components/chats/Chats.tsx
@@ -1,12 +1,16 @@
 import { Avatar, Icon } from "@/components/";
-import { CircleDashed } from "phosphor-react";
+import { CircleDashed, MagnifyingGlass } from "phosphor-react";
+import { Input } from "../ui/input";
 const Chats: React.FC = () => {
   return (
-    <div className="w-[300px] bg-slate-300/30 p-4 dark:bg-slate-800/70">
+    <div className="flex w-[300px] flex-col space-y-6 bg-slate-300/30 p-4 dark:bg-slate-800/70">
       <div className="flex flex-row items-center justify-between">
-        <h4 className="text-2xl font-bold">Chats</h4>
-        <div className="">asdfAAvatar</div>
+        <p className="text-2xl font-bold">Chats</p>
         <Icon Icon={CircleDashed} size={24} />
+      </div>
+      <div className=" flex w-full flex-row items-center rounded-full bg-blue-100 p-2 dark:bg-blue-900">
+        <MagnifyingGlass size={24} />
+        <Input />
       </div>
     </div>
   );
